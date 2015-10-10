afc580a50d65f5d322df34f9b5fda84a0785c32b
guybarrette <guy@guybarrette.com>
2015-10-10 12:41:40 -0400

    Commit initial

---
+++ BlinkyGHI/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using GHIElectronics.UWP.Shields;
+
+
+namespace App1
+{
+	/// <summary>
+	/// Demo #2
+	/// MSDEVMTL - October 6 2015
+	/// http://www.meetup.com/fr/msdevmtl/events/223839787/
+	/// 
+	/// 
+	/// This UWP project purpose is to demonstrate how to use the Windows 10 GPIO namespace to blink the red LED
+	/// on a FEZ Hat board attached to a Rapsberry Pi 2 this time, using GHI's driver code.
+	/// 
+	/// https://www.ghielectronics.com/catalog/product/500
+	/// 
+	/// </summary>
+	public sealed partial class MainPage : Page
+    {
+		
+		private FEZHAT hat;
+		private DispatcherTimer timer;
+		private bool next;
+		private int i;
+
+		public MainPage()
+        {
+            InitializeComponent();
+			Setup();
+        }
+
+
+
+		private async void Setup()
+		{
+			hat = await FEZHAT.CreateAsync();
+
+			hat.S1.SetLimits(500, 2400, 0, 180);
+			hat.S2.SetLimits(500, 2400, 0, 180);
+
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromMilliseconds(100);
+			timer.Tick += OnTick;
+			
+		}
+
+		/// <summary>
+		/// Start/Stop the timer
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+			if (timer.IsEnabled)
+			{
+				hat.DIO24On = false;
+				hat.D2.Color = FEZHAT.Color.Black;
+				hat.D3.Color = FEZHAT.Color.Black;
+				timer.Stop();
+				btnStartStop.Content = "Start";
+			}
+			else
+			{
+				timer.Start();
+				btnStartStop.Content = "Stop";
+			}
+
+		}
+
+		/// <summary>
+		/// Tick every 100 milliseconds to retrieve the button's state
+		/// but blink only every 1/2 second.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnTick(object sender, object e)
+		{
+			double x, y, z;
+
+			hat.GetAcceleration(out x, out y, out z);
+
+			LightTextBox.Text = hat.GetLightLevel().ToString("P2");
+			TempTextBox.Text = hat.GetTemperature().ToString("N2");
+			AccelTextBox.Text = $"({x:N2}, {y:N2}, {z:N2})";
+			Button18TextBox.Text = hat.IsDIO18Pressed().ToString();
+			Button22TextBox.Text = hat.IsDIO22Pressed().ToString();
+
+			if ((i++ % 5) == 0)
+			{
+				LedsTextBox.Text = next.ToString();
+
+				hat.DIO24On = next;
+				hat.D2.Color = next ? FEZHAT.Color.White : FEZHAT.Color.Black;
+				hat.D3.Color = next ? FEZHAT.Color.White : FEZHAT.Color.Black;
+
+				next = !next;
+			}
+
+		}
+
+
+	}
+}
---
+++ BlinkyGPIO/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace Blinky
+{
+	/// <summary>
+	/// Demo #1
+	/// MSDEVMTL - October 6 2015
+	/// http://www.meetup.com/fr/msdevmtl/events/223839787/
+	/// 
+	/// 
+	/// This UWP project purpose is to demonstrate how to use the Windows 10 GPIO namespace to blink the red LED
+	/// on a FEZ Hat board attached to a Rapsberry Pi 2.
+	/// 
+	/// https://www.ghielectronics.com/catalog/product/500
+	/// 
+	/// </summary>
+	public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 24; //Red LED
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DispatcherTimer timer;
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            InitGPIO();
+
+            if (pin != null)
+            {
+                timer.Start();
+            }        
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pin initialized correctly.";
+
+        }
+
+   
+		/// <summary>
+		/// Change the red LED value every 1/2 second
+		/// </summary>
+        private void Timer_Tick(object sender, object e)
+        {
+            if (pinValue == GpioPinValue.High)
+            {
+                pinValue = GpioPinValue.Low;
+                pin.Write(pinValue);
+                LED.Fill = redBrush;  //UI
+            }
+            else
+            {
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                LED.Fill = grayBrush;  //UI
+            }
+        }
+             
+
+    }
+}
